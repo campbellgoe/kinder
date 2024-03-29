fedea08fe9390f6957c0cba8a972dd098bdd5fdb
George Campbell <campbell.goe@gmail.com>
2024-03-29 19:54:56 +0000

    humpty dumpty sat on a wall

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
 import Link from "next/link";
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
+import { Octokit } from "@octokit/rest";
 
-export default function Home() {
+export default async function Home() {
+  const {
+    getUser,
+    isAuthenticated
+} = getKindeServerSession();
+const user = await getUser()
+// const octokit = new Octokit({
+//   auth: 'YOUR-TOKEN'
+// })
+
+// await octokit.request('GET /users/{username}', {
+//   username: 'USERNAME',
+//   headers: {
+//     'X-GitHub-Api-Version': '2022-11-28'
+//   }
+// })
   return (
     <div className="container">
+      <p>{await isAuthenticated() ? 'authenticated' : not}</p>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
       <div className="card hero">
         <p className="text-display-1 hero-title">
           Let’s start authenticating <br /> with KindeAuth
